19a257af92f8d8286ee4dcbb8175af7efe1f2fee
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-11-22 11:32:28 +0100

    use magic mock

--- python/tests/test_mail_sender.py
+++ python/tests/test_mail_sender.py
@@ -1,46 +1,55 @@
+from unittest.mock import MagicMock
 from ..mail_sender import MailSender, Request
+
 
 class Response:
     def __init__(self, code):
         self.code = code
 
+
 class HttpClient:
     def post(self, url, request):
         self.posted_url = url
         self.posted_request = request
-        
+
         return Response(200)
+
 
 class FailingHttpClient(HttpClient):
     def post(self, url, request):
         super().post(url, request)
-        
-        return Response(503)  
-        
+
+        return Response(503)
+
+
 class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
 
+
 def test_send_v1():
-    mail_sender = MailSender(HttpClient())
-    user = User("Hugo", "hugo@efrei.net")
+    http_client = MagicMock()
+    http_client.post = MagicMock(return_value=None)
+
+    mail_sender = MailSender(http_client)
+
+    user = MagicMock(name="Hugo", email="hugo@efrei.net")
     message = "Hello Hugo!"
-    
+
     mail_sender.send_v1(user, message)
-    
-    assert mail_sender.http_client.posted_request.name == "Hugo"
-    assert mail_sender.http_client.posted_request.email == "hugo@efrei.net"
+
+    http_client.post.assert_called_once_with(
+        mail_sender.base_url,
+        Request(user.name, user.email, "New notification", message),
+    )
 
 
 def test_send_v2():
     mail_sender = MailSender(FailingHttpClient())
     user = User("Hugo", "hugo@efrei.net")
     message = "Hello Hugo!"
-    
+
     mail_sender.send_v2(user, message)
 
-    assert type(mail_sender.http_client.posted_request) == Request
-
-    
-    
+    assert isinstance(mail_sender.http_client.posted_request, Request)
