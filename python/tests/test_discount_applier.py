7eaff395e783d9e7617e947eebb27caafce8c1f5
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-11-22 11:13:58 +0100

    Use magic mock

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,28 +1,36 @@
+from unittest.mock import MagicMock
 from ..discount_applier import DiscountApplier
+
 
 class User:
     def __init__(self):
         self.notification = None
 
+
 class Notifier:
     def notify(self, user, message):
         user.notification = message
 
+
 def test_apply_v1():
-    discount_applier = DiscountApplier(Notifier())
-    users = [User(), User(), User()]
-    
+    notifier = MagicMock()
+    notifier.notify = MagicMock(return_value=None)
+    discount_applier = DiscountApplier(notifier)
+    users = [MagicMock(), MagicMock(), MagicMock()]
+
     discount_applier.apply_v1(10, users)
-    
-    for user in users:
-        assert user.notification == "You've got a new discount of 10%"
+
+    expected_calls = expected_calls = [
+        ((user, "You've got a new discount of 10%"),) for user in users
+    ]
+    notifier.notify.assert_has_calls(expected_calls, any_order=True)
 
 
 def test_apply_v2():
     discount_applier = DiscountApplier(Notifier())
     users = [User(), User(), User()]
-    
+
     discount_applier.apply_v2(10, users)
-    
+
     for user in users:
         assert user.notification == "You've got a new discount of 10%"
