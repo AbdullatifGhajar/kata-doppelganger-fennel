a5f5bb333a80d71d9e3592778230f1bf1488a27b
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-11-22 11:20:21 +0100

    Refactor tests

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -2,39 +2,36 @@ from unittest.mock import MagicMock
 from ..discount_applier import DiscountApplier
 
 
-class User:
-    def __init__(self):
-        self.notification = None
+def assert_called_on_all_users(notifier, users):
+    expected_calls = expected_calls = [
+        ((user, "You've got a new discount of 10%"),) for user in users
+    ]
+    notifier.notify.assert_has_calls(expected_calls, any_order=True)
 
 
-class Notifier:
-    def notify(self, user, message):
-        user.notification = message
+def discount_applier_with_notifier():
+    notifier = MagicMock()
+    notifier.notify = MagicMock(return_value=None)
+    return notifier, DiscountApplier(notifier)
+
+
+def list_of_users():
+    return [MagicMock(), MagicMock(), MagicMock()]
 
 
 def test_apply_v1():
-    notifier = MagicMock()
-    notifier.notify = MagicMock(return_value=None)
-    discount_applier = DiscountApplier(notifier)
-    users = [MagicMock(), MagicMock(), MagicMock()]
+    notifier, discount_applier = discount_applier_with_notifier()
+    users = list_of_users()
 
     discount_applier.apply_v1(10, users)
 
-    expected_calls = expected_calls = [
-        ((user, "You've got a new discount of 10%"),) for user in users
-    ]
-    notifier.notify.assert_has_calls(expected_calls, any_order=True)
+    assert_called_on_all_users(notifier, users)
 
 
 def test_apply_v2():
-    notifier = MagicMock()
-    notifier.notify = MagicMock(return_value=None)
-    discount_applier = DiscountApplier(notifier)
-    users = [MagicMock(), MagicMock(), MagicMock()]
+    notifier, discount_applier = discount_applier_with_notifier()
+    users = list_of_users()
 
     discount_applier.apply_v2(10, users)
 
-    expected_calls = expected_calls = [
-        ((user, "You've got a new discount of 10%"),) for user in users
-    ]
-    notifier.notify.assert_has_calls(expected_calls, any_order=True)
+    assert_called_on_all_users(notifier, users)
