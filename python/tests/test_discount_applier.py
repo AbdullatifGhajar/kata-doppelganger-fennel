428cfe14b1ccacc81d2a37f051c635a127ff7b5a
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-11-22 10:14:25 +0100

    Add test

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,7 +1,21 @@
+from ..discount_applier import DiscountApplier
+
+class User:
+    def __init__(self):
+        self.notification = None
+
+class Notifier:
+    def notify(self, user, message):
+        user.notification = message
+
 def test_apply_v1():
-    # TODO: write a test that fails due to the bug in
-    # DiscountApplier.apply_v1
-    pass
+    discount_applier = DiscountApplier(Notifier())
+    users = [User(), User(), User()]
+    
+    discount_applier.apply_v1(10, users)
+    
+    for user in users:
+        assert user.notification == "You've got a new discount of 10%"
 
 
 def test_apply_v2():
