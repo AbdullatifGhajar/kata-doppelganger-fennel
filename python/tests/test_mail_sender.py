a30b60a72e2d2ce564fe80689c08961517e7e1d5
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-11-22 10:23:54 +0100

    Add test

--- python/tests/test_mail_sender.py
+++ python/tests/test_mail_sender.py
@@ -1,6 +1,24 @@
+from ..mail_sender import MailSender
+
+class HttpClient:
+    def post(self, url, request):
+        self.posted_url = url
+        self.posted_request = request
+        
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
 def test_send_v1():
-    # TODO: write a test that fails due to the bug in MailSender.send_v1
-    pass
+    mail_sender = MailSender(HttpClient())
+    user = User("Hugo", "hugo@efrei.net")
+    message = "Hello Hugo!"
+    
+    mail_sender.send_v1(user, message)
+    
+    assert mail_sender.http_client.posted_request.name == "Hugo"
+    assert mail_sender.http_client.posted_request.email == "hugo@efrei.net"
 
 
 def test_send_v2():
