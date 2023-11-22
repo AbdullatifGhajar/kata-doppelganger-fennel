9f63de42a0da5352e64b3a9b9bc4911c930c5c41
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2023-11-22 10:04:31 +0100

    Add test
    Co-authored-by: Stan97-2 <Stan97-2@users.noreply.github.com>

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,7 +1,16 @@
 import pytest
 
+from ..safe_calculator import SafeCalculator
 
+class AuthorizedAuthorizer:
+    def authorize(self):
+        return True
+    
 def test_divide_should_not_raise_any_error_when_authorized():
-    # TODO: write a test that fails due to the bug in
-    # SafeCalculator.add
-    pass
+    calculator = SafeCalculator(authorizer=AuthorizedAuthorizer())
+    
+    calculator.add(1, 2) # no error raised
+
+    
+    
+    
